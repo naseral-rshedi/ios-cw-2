967c4e7c707c6218a6978e8680a493b5fa746476
naser <bvif8n@gmail.com>
2020-06-20 20:37:47 +0300

    تم حل التمرين

---
+++ iOSClasswork2/ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+var numbre1 = 5
+var numbre2 = 6
+func add( a : Int, b : Int ) -> Int {
+   return a + b
+    
+}
+
+
+var functionCall =  add(a: numbre1, b: numbre2)
+   print(functionCall)
